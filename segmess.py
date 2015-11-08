e956609ba5aa300878a9da0890415892f41d6350
Baptiste <baptiste.deligny@protonmail.ch>
2015-11-08 23:50:28 +0100

    New segmess complete but needs debugging

--- segmess.py
+++ segmess.py
@@ -16,18 +16,28 @@ def segmess(message):
     f.close()
 
     fileSize = len(byteArr)
-    quotient = round(fileSize / 16 )
+    quotient = fileSize / 16
     reste = (fileSize % 16 )
     nbArr = quotient + reste
 
-    tabMess = [nbArr * [[4 * [0] , 4 * [0]]]]
+    tabMess = [nbArr * [4 * [ 4 * [0]]]]
 
-    bourrage = 16 - reste
-    for i in range(round($
+    init = 0
+    if reste != 0:
+        bourrage = 16 - reste
+        j = 0
+
+        for i in range(bourrage, 16):
+            print str(i/4) + " " + str(i%4) + " " + str(j)
+            tabMess[0][i/4][i%4] = byteArr[j]
+            print str(i) + " " + str(j)
+            j = j + 1
+        init=1
 
     l = reste
-    for i in range(1, quotient + 1 ):
+    for i in range(init, quotient + 1 ):
         for j in range(0, 4):
             for k in range(0, 4):
                 tabMess[i][j][k] = byteArr[l]
                 l = l + 1
+    return tabMess
