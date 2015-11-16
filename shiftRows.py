736bb02916e000724aeb223be6f3d2b3ad24adfe
Baptiste <baptiste.deligny@protonmail.ch>
2015-11-16 23:22:47 +0100

    function inShiftRows added, still need testing on both

--- shiftRows.py
+++ shiftRows.py
@@ -39,3 +39,39 @@ def decalage(message, permut):
     return tmp
 
 
+def invShiftRows(message, keySize):
+
+    #tests if the message is valid (i.e. a 4*4 array)
+    if len(message) == 4:
+        for i in range(4):
+            if len(message[i]) != 4:
+                raise ValueError('Bad message size in shiftRows(message, keysize)')
+    else:
+        raise ValueError('Bad message size in shiftRows(message, keysize)')
+
+    #test if the keysize is valid and initializes the corresponding permutations
+    if int(keySize) == 128:
+        permut = [0, 1, 2, 3]
+    elif keySize == 192:
+        permut = [0, 1, 2, 3]
+    elif keySize ==  256:
+        permut = [0, 1, 3, 4]
+    else:
+        raise ValueError('Bad key Size in shiftRows(message, keysize)')
+
+    for i in range(4):
+        message[i] =  decalageInv(message[i], permut[i])
+
+    return message
+
+def decalageInv(message, permut):
+    
+    tmp = [x for x in message]
+    tmp2 = [0, 0, 0, 0]
+    for i in range(permut):
+        for j in range(4):
+            tmp2[(3+j)%4] = tmp[j]
+        tmp = [x for x in tmp2]
+    
+    return tmp
+
