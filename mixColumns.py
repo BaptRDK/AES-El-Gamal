4c35ce0fb86b79a8cc5f61443c582e4262361c10
valentin8709 <valentin.dole@protonmail.com>
2015-12-08 16:55:55 +0100

    mixColumns: update subBytes: final release

--- mixColumns.py
+++ mixColumns.py
@@ -1,3 +1,18 @@
-#MixColumn function of the AES standard
-def mixColumns(message):
-    
+#! /usr/bin/python3.4
+
+# MixColumns Function
+
+from pylab import *
+
+def mixColumns(m):
+	
+	M_SIZE = 4
+	tab_a = [0 for row in range(M_SIZE)]	
+	tab_b = [0 for row in range(M_SIZE)]	
+
+	c = 0
+	h = 0
+
+	for cpt_l in range(M_SIZE):
+		tab_a[c] = m[c]
+		
--- subBytes.py
+++ subBytes.py
@@ -78,3 +78,4 @@ def subBytes(m):
 	print(tab_b)
 
 	return(tab_b)
+
