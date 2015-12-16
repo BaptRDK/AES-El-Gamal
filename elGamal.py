13dad357f4900d90a96b1f8232cee0bce1386d0f
Baptiste <baptiste.deligny@protonmail.ch>
2015-12-16 16:45:37 +0100

    commented version

--- elGamal.py
+++ elGamal.py
@@ -1,66 +1,59 @@
 from random import randrange
 import rabinMiller
 
-'''
-#calculate a generator for Z/Zn
-def calgene(n):
-    clef = int(n)
-    result = [0] * clef
-    tmp =-1
 
-    for i in range(2, clef):
-        j=1
-        k=1
-        while j <= (clef-1):
-            tmp = i**j % clef
-            if result[tmp] == 0:
-                result[tmp] = 1
-
-                if tmp == 1:
-                    while k <= (clef - 1) and result[k] != 0:
-                        k = k+1
-                    if k == clef:
-                        return(i)
-                    k = 1
-            j = j+1
-        result = [0] * clef
-        tmp = -1
-'''
-
-#trying the lagrange theoreme
+#using the lagrange theoreme, i.e. listing all the possible divisors
 def lagrange(n):
     clef = int(n)
     sousClef = clef - 1
     result = []
 
     #listing all divisor of sousClef
+    #for all number below (clef/2)
     for i in range(2, int((sousClef/2)+1)):
+
+        #check if the divides sousClef
         if ((sousClef / i ) % 1) == 0:
+            #building a list of all divisors
             result.append(i)
 
+    #adding sousClef as a divisor of sousClef to the list
     result.append(sousClef)
 
     return(result)
 
+#calculating one generator element for Z/nZ
 def calcGene(n):
     clef = int(n)
+    #getting the list of divisors for n
     diviseur = lagrange(clef)
+    #and the size of this list
     nbDiviseur = len(diviseur)
     sousClef = clef-1
 
+    #for each number between 2 and clef
     for i in range(2, clef):
         j = 0
+        
+        #do while j doesn't equal the number of divisor (we don't have tested all the divisor for this i)
         while j < nbDiviseur:
+            #calculating the order of this i
             tmp = (i**diviseur[j]) % clef
             
+            #when we find it's order
             if tmp == 1:
+                #we check if it's order is equal to sousClef (i.e. every elements of the group has been generated)
                 if diviseur[j] == sousClef:
+                    #if so i is a generator
                     return(i)
+
+                #if not we change j to go on with the next i
                 else:
                     j = nbDiviseur
 
             j = j+1
-#generate and store in a file a 256-bits couple of keys
+
+#generate and store in several files the differents component of the keys
 def keygen():
 
     tailleClef = 16
