c9c632f24c56cde3f41fbadbc794c975121e2f83
Baptiste <baptiste.deligny@protonmail.ch>
2016-01-03 16:44:09 +0100

    working version of elGamal (full)

--- elGamal.py
+++ elGamal.py
@@ -1,5 +1,8 @@
 from random import randrange
+from os import path
+import pickle
 import rabinMiller
+from confElGamal import tailleClef, nomClef
 
 
 #using the lagrange theoreme, i.e. listing all the possible divisors
@@ -56,32 +59,135 @@ def calcGene(n):
 #generate and store in several files the differents component of the keys
 def keygen():
 
-    tailleClef = 16
+    print("Generating keys, this could take a while...")
     clef = randrange(0, 2**tailleClef)
 
     while not rabinMiller.isPrime(clef):
         clef = randrange(2, 2**tailleClef)
     
     #write the first part of the public key into a file
-    file = open("clefQ", 'w')
+    file = open(nomClef[0], 'w')
     file.write(str(clef))
     file.close()
 
     #write the second part of the public key into another file
     generator = calcGene(clef)
-    file = open("clefP", 'w')
+    file = open(nomClef[1], 'w')
     file.write(str(generator))
     file.close()
 
     #randomly generate the secret key, it must be <clefQ
     secret = randrange(1, clef)
-    file = open("clefS", 'w')
+    file = open(nomClef[2], 'w')
     file.write(str(secret))
     file.close()
 
     #calculate the third part of the public key and write it into a fourth file
-    file = open("clefH", 'w')
+    file = open(nomClef[3], 'w')
     file.write(str((generator**secret)%clef))
     file.close()
 
+#encrypt the file fic
+def chiffreFic(fic):
 
+    cible = str(fic)
+    
+    #test if the file exists
+    if not path.isfile(cible):
+        raise ValueError("ElGamal: invalid entry file")
+    
+    #test if the pubic key exists
+    if not path.isfile(nomClef[3]) or not path.isfile(nomClef[0]) or not path.isfile(nomClef[1]):
+        rep = input("Keys not found, do you wish to generate them ?")
+        rep = rep.lower()
+        
+        #if not we can generate it
+        if rep == "y" or rep == "o":
+            keygen()
+        else:
+            raise ValueError("ElGamal: Keys not found")
+
+    #opening and storing the 3 components of the public key
+    f = open(nomClef[0], "r")
+    clefQ = int(f.read())
+    f.close()
+
+    f = open(nomClef[3], "r")
+    clefH = int(f.read())
+    f.close()
+    
+    f = open(nomClef[1], "r")
+    clefG = int(f.read())
+    f.close()
+
+    #get the file in binary and map it before closing it
+    f = open(cible, "rb")
+    byteArr = list(f.read())
+    f.close()
+    
+    c1c2 = [] 
+    
+    #encrypting each byte of the file
+    for byte in byteArr:
+        #random Y for each byte(block)
+        clefY = randrange(1, clefQ)
+        #first we happen c1
+        c1c2.append((clefG**clefY) % clefQ)
+
+        secret = (clefH**clefY) % clefQ
+        
+        #then c2
+        c1c2.append((byte*secret))
+    
+    
+    result = cible + ".chif"
+    
+    #creating the encrypted file
+    f = open(result, "wb")
+    pickle.dump(c1c2, f)
+
+#decrypting function
+def deChiffreFic(fic):
+    cible = str(fic)
+    result = cible + ".clr"
+
+    #test if the file exists
+    if not path.isfile(cible):
+        raise ValueError("ElGamal: invalid entry file")
+
+    #test if the pubic key exists
+    if not path.isfile(nomClef[0]) or not path.isfile(nomClef[2]):
+        raise ValueError("ElGamal: secret key not found")
+
+    #opening and storing the public key
+    f = open(nomClef[2], "r")
+    secret = int(f.read())
+    f.close()
+
+    f = open(nomClef[0], "r")
+    clefQ = int(f.read())
+    f.close()
+
+    f = open(cible, "rb")
+    crypt = pickle.load(f)
+    f.close()
+
+    i = 0
+    clair = [] 
+
+    
+    while i < len(crypt):
+        #decrypting
+        clair.append(int(crypt[i+1]/((crypt[i]**secret)%clefQ)))
+        i = i + 2
+
+    #opening a new file for the decrypted file
+    f = open(result, "wb")
+    f.write(bytes(clair))
+    f.close()
+
+
+
+
+
+    
